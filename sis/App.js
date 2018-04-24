df49b472d90617fe3107fb6b492bd10908a8a46e
Olumide Igbiloba <enocholumide@gmail.com>
2018-04-24 21:39:59 +0200

    Link Native base styles

--- sis/App.js
+++ sis/App.js
@@ -1,14 +1,35 @@
+import {Container, StyleProvider, Content} from 'native-base';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import getTheme from './native-base-theme/components';
+
 export default class App extends React.Component {
+
+  constructor() {
+  super();
+  this.state = {
+    isReady: false
+  };
+  }
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("native-base/Fonts/Ionicons.ttf")
+    });
+    this.setState({ isReady: true });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <StyleProvider  style={getTheme()}>
+        <View style = {styles.container}>
+           <Text>Open up App.js to start working on your app!</Text>
+           <Text>Changes you make will automatically reload.</Text>
+           <Text>Shake your phone to open the developer menu.</Text>
+         </View>
+      </StyleProvider>
     );
   }
 }
